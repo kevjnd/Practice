47bfd931205a98923198bb267ea21043d55462d3
kevjnd <94998639+kevjnd@users.noreply.github.com>
2025-03-02 02:16:16 +0200

    Upgraded the riddle game

--- Riddle.py
+++ Riddle.py
@@ -1,161 +1,390 @@
 import random
+import os
 
 class RiddleGame:
     def __init__(self):
-        # List of riddles and answers
+        # List of riddles with their answers and hints
         self.riddles = [
-            {
-                "question": "I am light as a feather, yet the strongest man can't hold me for much longer. What am I?",
-                "answer": "breath",
-                "hint": "It's something you do to stay alive."
-            },
             {
                 "question": "What is always in front of you but can't be seen?",
                 "answer": "future",
-                "hint": "It's the time that is yet to come."
+                "hints": [
+                    "It's the time that is yet to come.",
+                    "You can't touch it, but you know it's coming."
+                ]
             },
             {
                 "question": "I can talk, but I don't have a mouth. I can listen, but I don't have ears. What am I?",
                 "answer": "telephone",
-                "hint": "You use it to communicate."
+                "hints": [
+                    "You use it to communicate.",
+                    "It allows you to talk to someone far away."
+                ]
             },
             {
                 "question": "I am always hungry, but I never eat. I drink a lot, but never thirst. What am I?",
                 "answer": "fire",
-                "hint": "It can be dangerous if not controlled."
+                "hints": [
+                    "It can be dangerous if not controlled.",
+                    "It needs oxygen to keep burning."
+                ]
             },
             {
                 "question": "I can't move, but I always move forward. What am I?",
                 "answer": "time",
-                "hint": "It keeps ticking even if you don't notice."
+                "hints": [
+                    "It keeps ticking even if you don't notice.",
+                    "It’s often measured in hours, minutes, and seconds."
+                ]
             },
             {
                 "question": "The more of this there is, the less you see. What is it?",
                 "answer": "darkness",
-                "hint": "It's the opposite of light."
+                "hints": [
+                    "It's the opposite of light.",
+                    "When the sun sets, it appears."
+                ]
             },
             {
                 "question": "What has keys but can't open locks?",
                 "answer": "piano",
-                "hint": "It makes sound when played."
+                "hints": [
+                    "It makes sound when played.",
+                    "You play it by pressing black and white keys."
+                ]
             },
             {
                 "question": "What has a head, a tail, but no body?",
                 "answer": "coin",
-                "hint": "It's something you flip."
+                "hints": [
+                    "It's something you flip.",
+                    "It has two sides, heads and tails."
+                ]
             },
             {
                 "question": "What can travel around the world while staying in the corner?",
                 "answer": "stamp",
-                "hint": "It sticks to something to help it travel."
+                "hints": [
+                    "It sticks to something to help it travel.",
+                    "You place it on envelopes."
+                ]
             },
             {
                 "question": "I am tall when I am young, and I am short when I am old. What am I?",
                 "answer": "candle",
-                "hint": "I provide light."
+                "hints": [
+                    "I provide light.",
+                    "I burn down as I age."
+                ]
             },
             {
                 "question": "What has a heart that doesn't beat?",
                 "answer": "artichoke",
-                "hint": "It's a vegetable."
+                "hints": [
+                    "It's a vegetable.",
+                    "It’s often used in cooking."
+                ]
             },
             {
                 "question": "The more you take, the more you leave behind. What am I?",
                 "answer": "footsteps",
-                "hint": "You leave these when you walk."
+                "hints": [
+                    "You leave these when you walk.",
+                    "They are left in the sand or snow."
+                ]
             },
             {
                 "question": "What comes down but never goes up?",
                 "answer": "rain",
-                "hint": "It falls from the sky."
+                "hints": [
+                    "It falls from the sky.",
+                    "It’s often part of a weather forecast."
+                ]
             },
             {
                 "question": "What can be cracked, made, told, and played?",
                 "answer": "joke",
-                "hint": "It makes people laugh."
+                "hints": [
+                    "It makes people laugh.",
+                    "People often tell them to lighten the mood."
+                ]
             },
             {
                 "question": "What has one eye but can't see?",
                 "answer": "needle",
-                "hint": "It's used in sewing."
+                "hints": [
+                    "It's used in sewing.",
+                    "It’s sharp at one end and used with thread."
+                ]
             },
             {
                 "question": "What begins with T, ends with T, and has T in it?",
                 "answer": "teapot",
-                "hint": "You use this to make tea."
+                "hints": [
+                    "You use this to make tea.",
+                    "It has a handle and a spout."
+                ]
             },
             {
                 "question": "What is full of holes but still holds a lot of weight?",
                 "answer": "sieve",
-                "hint": "You use it in cooking to strain liquids."
+                "hints": [
+                    "You use it in cooking to strain liquids.",
+                    "It’s often made of metal or plastic."
+                ]
             },
             {
                 "question": "What has hands but can't clap?",
                 "answer": "clock",
-                "hint": "It helps you keep track of time."
+                "hints": [
+                    "It helps you keep track of time.",
+                    "It usually has numbers and moving hands."
+                ]
             },
             {
                 "question": "What has a neck but no head?",
                 "answer": "bottle",
-                "hint": "You can fill it with liquids."
+                "hints": [
+                    "You can fill it with liquids.",
+                    "It has a narrow opening at the top."
+                ]
             },
             {
                 "question": "What can you catch but not throw?",
                 "answer": "cold",
-                "hint": "It makes you feel unwell."
+                "hints": [
+                    "It makes you feel unwell.",
+                    "You can catch it from others, especially in winter."
+                ]
             }
         ]
         self.score = 0
+        self.hint_counter = {}
+
+        # ASCII art
+        self.question_mark_art = """
+             ________
+        _jgN########Ngg_
+      _N##N@@""  ""9NN##Np_
+     d###P            N####p
+     "^^"              T####
+                       d###P
+                    _g###@F
+                 _gN##@P
+               gN###F"
+              d###F
+             0###F
+             0###F
+             0###F
+             "NN@'
+
+              ___
+             q###r
+              ""
+        """
+        self.fox_art = """
+        /\   /\  
+       //\\_//\\     ____
+       \_     _/    /   /
+        / * * \    /^^^]
+        \_\O/_/    [   ]
+         /   \_    [   /
+         \     \_  /  /
+          [ [ /  \/ _/
+         _[ [ \  /_/  
+        """
+        self.rose_art = """
+       .      .'
+        :`...' `.,'  ' 
+    `.  ' .**.  ; ; ':
+    ` ``:`****,'  .' :
+  ..::.  ``**":.''   `.
+.:    `: ; `,'        :
+  `:    `   :         ;
+    :   :   :        ;
+    :    :   :     .:
+     :    :   :..,'  ``::.
+      `....:..'  ..:;''
+      .:   . ...::::
+     ,'''''``:::::::
+               `::::
+                 `::.
+                  `::
+           . ,.    ::::'      ,.. 
+         .'.'  ``.  ::      .'.. `.
+        '        .: ::    ,'.'     .
+      .' ,'    .::::::   ,.'    .:::.
+    .' .'  ..:'     ::: .,   .;'     ~
+   ,;::;.::''        ::.:..::'
+  ~                  ::;'
+                     ::
+                   ,:::
+                     ::.
+                     `::
+                      ::
+                      ::
+                      ::
+                      ::
+                      ::
+                      ::
+                      ::
+        """
+        self.mushroom_art = """
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⡶⢶⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣀⠴⠛⠛⠉⠄⠀⠀⠀⠀⠀⠀⠀⠈⠑⠒⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣤⡊⠁⣀⣤⣄⣤⣤⣤⣀⣤⣤⣤⣀⣤⣠⣶⣠⣀⠉⠢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⡠⠋⢀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠐⣡⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⡹⣽⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣟⣀⣠⡤⠶⢄⡀⠲⢄⠀⠀⠀⠀⠀⠀
+⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡴⠶⣾⣿⣿⣿⣿⡿⠛⠛⠉⠉⠁⠀⠉⠀⠀⠀⠀⠈⠉⠒⢷⣄⠀⠀⠀⠀
+⠀⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠘⣿⡿⢿⣯⣶⣶⣶⣶⣠⣴⣂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀
+⠀⠀⠀⠀⠀⠉⠉⠉⠛⠋⠉⠉⡀⠀⠀⠹⡀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣺⢶⣶⣤⣀⡀⠀⠀⠘⣆⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢧⠀⠀⠂⣧⠀⠀⠉⠛⠻⠿⣿⣿⣿⣿⠟⠉⠛⣾⣿⣿⣷⣶⣄⡀⠘⢆⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠈⠉⢻⠏⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣶⣼⡆
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠸⡇⠀⠀⠀⠀⠀⠰⣖⠾⢧⣴⣲⣿⡏⠉⠛⠛⠻⠿⠿⣿⠿⠃
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠂⠄⠀⢄⣧⠀⠀⠀⠀⠀⠀⠈⢹⠷⣶⡿⣯⣵⡤⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⡀⢆⢠⣿⠀⠀⠀⠀⠀⠀⡰⠋⠠⣿⣷⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣷⠿⡿⢹⣿⣆⠀⠀⠀⠀⡰⠉⠉⢹⡟⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣿⣖⠘⣾⣹⡇⠀⠀⠀⡸⠁⠀⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣨⣾⣿⣧⡀⠀⠀⣇⠐⠀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢛⣿⠿⣄⣾⢷⠦⠀⢸⡿⢧⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢀⣿⣟⡾⢿⣿⣷⢽⡍⢷⣯⣿⠓⢤⣒⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢠⣴⡶⣀⠀⠀⢸⡽⠂⢸⣿⣽⣻⣿⣤⣾⣿⣿⣿⣿⣄⡀⡸⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠫⡁⠀⢻⠿⢷⣤⣿⣿⣶⣿⣿⣿⣯⣿⣻⣿⣿⣶⣠⣳⠀⠀⠐⣿⡿⠄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⠲⠞⠒⠛⠛⠛⠛⠛⠋⠻⠛⠓⠛⠛⠛⠛⠛⠛⠓⠚⠓⠛⠛⠂⠀⠀⠀⠀⠀⠀⠀
+        """
+        self.sheep_art = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⡿⠿⢿⣦⡀⢀⣴⡿⠿⠿⢷⣄⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠁⠀⠀⠀⠙⢿⡿⠋⠀⠀⠀⠈⢿⣦⡾⠟⠋⠻⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⡶⠾⠿⢶⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠋⠁⠀⠀⠀⣿⣗⣤⣤⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⣤⡴⠶⠒⠒⠲⢦⣤⡀⠀⠀⠀⠀⠀⣴⡿⠉⠀⠀⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠉⠉⢿⡆⠀⠀⠀⠀⠀
+⠀⣤⡞⠋⠁⠀⠀⠀⠀⠀⠀⡈⢙⣷⣴⡶⠶⠶⠿⢷⣦⣄⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡇⠀⠀⠀⠀⠀
+⢠⣿⠀⠀⠀⠀⠀⢀⣀⣤⣤⣶⣿⡋⠁⠀⠀⠀⠀⠀⠈⠙⢷⣄⢀⣤⡶⠞⠋⠉⠉⠙⠛⠲⢦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡿⠷⠶⣶⣄⠀⠀
+⠀⠙⠿⢦⣤⡴⠾⠋⠁⣰⣿⣿⠟⠛⠛⠿⣦⠀⣀⣀⣀⣀⡀⠿⣿⡁⠠⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣿⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⢟⢸⡇⠀⡀⣤⣄⣹⡿⠯⠁⠀⠈⠻⣦⣸⣿⠛⠛⠻⠶⣤⣀⠀⠀⠀⢀⣸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠟⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣼⡟⠀⠸⣷⣄⠀⢟⣇⣿⡇⣾⣷⠀⠀⠀⢸⡟⣿⡄⠀⠀⠀⠀⠈⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠻⠿⣷⣆
+⠀⠀⠀⠀⠀⠀⣼⡟⠐⠀⠀⠀⠉⠛⠛⠉⠸⣧⣈⠁⠀⠀⣠⡾⠃⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿
+⠀⠀⠀⠀⠀⢠⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠓⠒⠚⠋⠀⢀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡾⠟
+⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣯⡥⠀
+⠀⠀⠀⠀⠀⢸⣿⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⠀
+⠀⠀⠀⠀⠀⠸⣿⡄⠀⣿⡟⠀⣴⣤⠀⠀⠀⠀⠀⠀⠀⢀⣴⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣶⡿⠆
+⠀⠀⠀⠀⠀⠀⠙⣿⣆⠊⠁⠀⠉⠉⠀⠀⠀⠀⠀⣠⣶⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠹⣿⡉⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠻⢷⣤⣤⣤⣤⣤⣤⣶⢾⣿⣯⣥⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⢀⣿⠇⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⣹⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠻⠟⠋⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣷⣤⣴⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠀⠀⠀⢀⣿⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡁⠀⢀⣴⣶⠀⠀⠀⢠⠀⠀⠀⢀⣄⠀⠀⠀⢸⣧⠀⠀⠀⣿⣿⣶⡾⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡇⠻⡿⢻⣿⣿⡀⢀⣴⣿⣇⠀⢀⣾⣿⣶⣤⣤⣿⣿⣷⣶⣾⢿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇⣇⠀⣇⢸⡇⠉⠙⠛⠉⠘⠛⠿⠛⠋⠀⠉⠉⠉⢹⣯⣷⠀⢻⡘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⢻⠀⣿⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣨⣿⣿⡆⠸⡇⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣿⢸⡇⢹⠸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡶⣟⣫⣼⠟⠃⠀⣷⠸⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣟⣫⣴⠾⠟⠁⢸⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠛⠁⠀⠀⠀⠀⣿⡠⣿⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠃⠀⠀⠀⠀⠀⠀⢸⡄⢿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠾⠟⣋⣤⡾⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠿⠶⠶⠚⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        """
+
+        self.welcome_art = """
+        ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+        │           Welcome to Riddle Game!        │
+        │     Solve riddles, earn points, and      │
+        │   become the ultimate Riddle Master!     │
+        ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+        """
+
+    def clear_screen(self):
+        """Clear the screen for a cleaner interface."""
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def display_score(self):
+        """Display the current score."""
+        print(f"\033[1;33mYour current score: {self.score}\033[0m")
+        print("=" * 50)
+
+    def check_exit(self, user_input):
+        """Check if the user wants to exit."""
+        if user_input == "EXIT":
+            print("\033[1;31mExiting the game... Thank you for playing!\033[0m")
+            exit()  # Exit the game
 
     def ask_riddle(self):
+        """Ask the riddle to the user."""
         riddle = random.choice(self.riddles)  # Chooses a random riddle
-        print(f"Try to solve this riddle:\nRiddle: {riddle['question']}")
-        user_answer = input("Your answer: ").strip().lower()
+        display_art = random.choice([self.question_mark_art, self.fox_art, self.rose_art, self.mushroom_art, self.sheep_art, None])  # Randomly choose art
 
-        if user_answer == riddle["answer"]:
-            print("Correct answer! Well done!")
-            self.score += 1
-        else:
-            print(f"Incorrect answer!")
-            self.provide_options(riddle)
+        self.clear_screen()
 
-    def provide_options(self, riddle):
-        print("\nWhat would you like to do next?")
-        print("1. Try again")
-        print("2. Skip this riddle")
-        print("3. Show the correct answer")
-        print("4. Get a hint")
-        choice = input("Enter the number of your choice (1/2/3/4): ").strip()
+        # Print the random ASCII art (Question Mark, Fox, or Rose)
+        if display_art:
+            print(display_art)
 
-        if choice == "1":
-            print("Let's try again!\n")
-            self.ask_riddle()
-        elif choice == "2":
-            print("You skipped the riddle.\n")
-        elif choice == "3":
-            print(f"The correct answer was: {riddle['answer']}\n")
-        elif choice == "4":
-            print(f"Hint: {riddle['hint']}\n")
-        else:
-            print("Invalid choice. Please choose a valid option (1/2/3/4).\n")
+        print("\033[1;36m*******************\033[0m")
+        print("\033[1;35m   RIDDLE GAME   \033[0m")
+        print("\033[1;36m*******************\033[0m")
+
+        print(self.welcome_art)  # Always show this only at the beginning
+
+        print("\033[1;32mTry to solve this riddle:\033[0m")
+        print(f"\033[1;34mRiddle: {riddle['question']}\033[0m")
+
+        while True:  # Loop to allow user to retry the riddle after hint
+            # Ask for user input and check if they want to exit
+            user_answer = input("\nYour answer (or type 'HINT' for a hint, 'EXIT' to quit): ").strip().lower()
+            self.check_exit(user_answer.upper())
+
+            if user_answer == "hint":
+                self.show_hint(riddle)  # Show hint and let them guess again
+                print(f"\033[1;34mRiddle: {riddle['question']}\033[0m")  # Display the riddle again
+                continue  # Allow the user to guess the same riddle again after hint
+
+            if user_answer == riddle["answer"]:
+                self.score += 1  # Update score immediately
+                self.clear_screen()
+                self.display_score()
+                print("""
+        ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+        │       Correct Answer!       │
+        │  You're on fire, well done!  │
+        ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+                """)
+                input("\nPress [ENTER] to continue to the next riddle...")  # Wait for user to press ENTER to continue
+                break  # Exit the loop and move to the next riddle
+            else:
+                self.clear_screen()
+                self.display_score()
+                print("""
+        ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
+        │     Incorrect answer!       │
+        │    The correct answer is:   │
+        │    \033[1;33m{}\033[0m              │
+        ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+                """.format(riddle["answer"]))
+                input("\nPress [ENTER] to continue to the next riddle...")  # Wait for user to press ENTER to continue
+                break  # Exit the loop and move to the next riddle
+
+    def show_hint(self, riddle):
+        """Show a hint for the riddle."""
+        if riddle["question"] not in self.hint_counter:
+            self.hint_counter[riddle["question"]] = 0
+
+        # Get the hint based on how many times the user has asked for a hint
+        hint_index = self.hint_counter[riddle["question"]] % len(riddle["hints"])
+        print(f"\033[1;36mHint: {riddle['hints'][hint_index]}\033[0m\n")
+        self.hint_counter[riddle["question"]] += 1
+
+        # After showing the hint, let the user try again
+        input("\nPress [ENTER] to attempt the riddle again...")  # Wait for user to press ENTER before continuing
 
     def start_game(self):
-        print("Welcome to the Riddle Game! Try to solve as many riddles as you can.")
+        """Start the Riddle Game."""
+        self.clear_screen()
+        print(self.welcome_art)  # Display welcome ASCII art when game starts
+        print("Ready to begin? Solve as many riddles as you can!")
+        input("\nPress [ENTER] to start...")  # Wait for user to press ENTER to start the game
+
+        # Start asking riddles
         while True:
             self.ask_riddle()
-            play_again = input("Do you want to continue playing? (yes/y or no/n, type 'exit' to quit): ").strip().lower()
 
-            if play_again == "exit":
-                print(f"Game over. Your final score is: {self.score}")
-                break
-            elif play_again in ["yes", "y"]:
-                continue
-            elif play_again in ["no", "n"]:
-                print(f"Game over. Your final score is: {self.score}")
-                break
-            else:
-                print("Invalid input. To exit, type 'exit', or to continue, type 'yes/y' or 'no/n'.\n")
-
+# Main function to start the game
 if __name__ == "__main__":
     game = RiddleGame()
     game.start_game()
